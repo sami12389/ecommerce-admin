02f58e8965ff474cf05a243912121e1b810e2d61
sami12389 <samseifu974@gmail.com>
2024-01-02 19:03:39 -0500

    Bugs

--- app/(dashboard)/[storeId]/(routes)/orders/components/clients.tsx
+++ app/(dashboard)/[storeId]/(routes)/orders/components/clients.tsx
@@ -1,6 +1,5 @@
 "use client"
 import { DataTable } from "@/components/ui/data-table"
-
 import { Heading } from "@/components/ui/heading"
 import { Separator } from "@/components/ui/separator"
 import {OrderColumn, columns} from "./columns"
--- components/MainNav.tsx
+++ components/MainNav.tsx
@@ -59,7 +59,7 @@ const MainNav = ({
         key = {route.href} 
         href = {route.href}
         className = {cn(
-          "text-sm font-medium transition-colors hover:text-primary",
+          "text-sm font-medium transition-colors uppercase hover:text-primary",
           route.active ? "text-black dark:text-white" : "text-muted-foreground"
         )}
         >
